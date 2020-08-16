ebec99130c3faa88df826456e730f9b1650538ae
Corey Quon <corey.quon@gmail.com>
2020-08-16 11:50:22 -0700

    Add basic navigation bar

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,20 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+const ListLink = (props) => (
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link
+      to={props.to}
+      style={{
+        color: `white`,
+        textDecoration: `none`,
+      }}
+    >
+      {props.children}
+    </Link>
+  </li>
+)
+
 const Header = ({ siteTitle }) => (
   <header
     style={{
@@ -27,6 +41,10 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
+      <ul style={{ listStyle: `none`, float: `right` }}>
+        <ListLink to="/">Home</ListLink>
+        <ListLink to="/about/">About</ListLink>
+      </ul>
     </div>
   </header>
 )
